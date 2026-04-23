1b30e796294d1b554c32748b263ca8d171e4dc35
Ayomide2604 <theolowuayo@gmail.com>
2026-04-23 17:37:53 -0600

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,9 +63,23 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="theme-color" content="#0171c5" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicons/favicon-16x16.png"
+        />
         <link rel="shortcut icon" href="/favicons/favicon.ico" />
         <link rel="manifest" href="/favicons/manifest.json" />
         <link rel="stylesheet" href="/assets/css/bootstrap.min.css" />
